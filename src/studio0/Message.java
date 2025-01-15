7fb3ccf9c644354f2c5df2d42e11d974f6427503
coraxcorvus <coraxcorvus@localhost>
2025-01-15 14:13:41 -0600

    asdf

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println(" Hello Amelia and Brenda"+ " - Harry");
+		System.out.println(" Hello Amelia and Brenda"+ " -From Harry");
 	}   
 }
